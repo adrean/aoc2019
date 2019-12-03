a7a3303672d499565c8a5721725245bea6371c1f
Anthony Drean <anthony.drean@xandr.com>
2019-12-03 09:06:36 +0100

    Day 3 solution - distances class

---
+++ scala/src/main/scala/com/adrean/aoc2019/utils/Distance.scala
@@ -0,0 +1,11 @@
+package com.adrean.aoc2019.utils
+
+object Distance {
+
+  def manhattanDistance(a: (Int, Int), b: (Int, Int)): Int = {
+    scala.math.abs(a._1 - b._1) + scala.math.abs(a._2 - b._2)
+  }
+
+  def manhattanDistanceToOrigin(a:(Int,Int)): Int = manhattanDistance(a, (0,0))
+
+}
