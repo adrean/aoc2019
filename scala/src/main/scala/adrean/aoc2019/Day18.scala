983fbfbcfdc822e43dcca5c6845c53e3f4b26c74
Anthony Drean <anthony.drean@xandr.com>
2020-01-03 17:40:59 +0100

    Init Day 18, will continue later

---
+++ scala/src/main/scala/adrean/aoc2019/Day18.scala
@@ -0,0 +1,10 @@
+package adrean.aoc2019
+
+object Day18 {
+
+  def main(args: Array[String]): Unit = {
+    val source = "dat18.txt"
+    val data = scala.io.Source.fromResource(source).getLines().toArray
+  }
+
+}
